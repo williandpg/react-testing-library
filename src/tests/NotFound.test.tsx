f77ab3054dba2118b982557200bcf8487d20c10a
Willian Gonçalves <willian.p.g@outlook.com>
2023-06-27 15:16:59 -0300

    Requisito 4

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const navNotFoundPage = getByRole('heading', { name: /page requested not found/i });
+  expect(navNotFoundPage).toBeInTheDocument();
+});
+
+test('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const imgNotFoundPage = getByRole('img');
+  expect(imgNotFoundPage).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
